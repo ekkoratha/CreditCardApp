0751d9abab918265cf0a877b933b15e00e6ac650
ekkoratha <ekkoratha@gmail.com>
2022-03-27 21:59:01 +0100

    ver 2 commenting out WebConfig as its disabling swagger

--- src/main/java/com/ps/creditcardapp/configuration/WebConfig.java
+++ src/main/java/com/ps/creditcardapp/configuration/WebConfig.java
@@ -5,14 +5,14 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-@EnableWebMvc
+//@Configuration
+//@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
     // Cross-Origin Resource Sharing (CORS)
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry
-                .addMapping("/**")
-                .allowedOrigins("http://localhost:8080");
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry
+//                .addMapping("/**");
+//                //.allowedOrigins("http://localhost:8080");
+//    }
 }
